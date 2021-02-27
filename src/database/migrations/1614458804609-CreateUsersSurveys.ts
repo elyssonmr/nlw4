b473d3ef1f7eea491ce4d8ab588797b22d4c51c3
Elysson MR <elyssonmr@gmail.com>
2021-02-27 17:47:41 -0300

    Fix repo classes

---
+++ src/database/migrations/1614458804609-CreateUsersSurveys.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsersSurveys1614458804609 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
